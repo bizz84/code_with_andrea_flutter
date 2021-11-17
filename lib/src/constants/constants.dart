f0eaeaf73f738bb657f05a103097a1945134f47e
Andrea Bizzotto <codewithandrea@gmail.com>
2021-11-17 09:30:26 +0000

    Use WebP images

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -4,18 +4,18 @@ class Constants {
   static const search = 'assets/search.png';
   static const hamburgerMenu = 'assets/hamburger-menu.png';
 
-  static const tutorial1 = 'assets/how-to-parse-json.png';
-  static const tutorial2 = 'assets/side-effects.png';
-  static const tutorial3 = 'assets/split-view.png';
-  static const tutorial4 = 'assets/learn-flutter-animations.png';
+  static const tutorial1 = 'assets/how-to-parse-json.webp';
+  static const tutorial2 = 'assets/side-effects.webp';
+  static const tutorial3 = 'assets/split-view.webp';
+  static const tutorial4 = 'assets/learn-flutter-animations.webp';
 
-  static const dartCourse = 'assets/dart-course-banner-small.png';
+  static const dartCourse = 'assets/dart-course-banner-small.webp';
   static const flutterAnimationsCourse =
-      'assets/flutter-animations-course-banner-small.png';
+      'assets/flutter-animations-course-banner-small.webp';
   static const flutterFirebaseCourse =
-      'assets/flutter-firebase-course-banner-small.png';
+      'assets/flutter-firebase-course-banner-small.webp';
   static const flutterRestApiCourse =
-      'assets/flutter-rest-api-course-banner-small.png';
+      'assets/flutter-rest-api-course-banner-small.webp';
 
   static const iconStar = 'assets/icon-star.svg';
   static const andreaAvatar = 'assets/andrea-avatar.webp';
